da9dcbba665a2c9d5dffbf671c2a9d521996e0d4
Pedro Prieto <pedro.prieto003@gmail.com>
2020-03-16 19:49:22 -0400

    day 1 mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,96 @@
+import random
+
+my_arr = [random.randint(0,5) for _ in range(6)]
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        for j in range(i,len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
-
-
-        # TO-DO: swap
-
-
-
+        temp = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = temp
 
     return arr
+
+#print(selection_sort([random.randint(0,22) for _ in range(30)]))
+
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    sorted_index = len(arr) - 1
+    while sorted_index > 0:
+
+        for i in range(sorted_index):
+
+            if(arr[i] > arr[i + 1]):
+                temp = arr[i + 1]
+                arr[i+1] = arr[i]
+                arr[i] = temp
+            
+        sorted_index -= 1
+
+
 
     return arr
+
+#print(selection_sort([random.randint(0,22) for _ in range(30)]))
+
+
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort( arr, maximum=-1 ): #arr len = N  K
+    ## An array of the length of the range of the incoming arr
+    my_arr = [0] * (maximum + 1)
 
+    ## Increment the value in my_arr at the index of the value of arr[i]
+    ## It counts how many times the number occurs
+    for i in range(len(arr)):
+        my_arr[arr[i]] += 1
+
+
+    
+    #### RETURNING NEW ARR 
+    ## SPACE COMPLEXITY OF (n + k) k = range    n = len of arr
+    ## TIME COMPLEXITY OF (n + k) k = range   n = len of arr
+    # Make a new array of length of the original arr
+    # Decrement my_arr[i] by one
+    # Look at the starting index of arr[i] in my_arr
+    # change returning_arr at the index we got to the value of arr[i]
+
+    ## Loop through my arr and add the value of the index before to the current index 
+    ## How many elements come before the current element
+    # for i in range(1,len(my_arr)):
+    #     my_arr[i] += my_arr[i - 1]
+
+    # returning_arr = [0] * len(arr)
+    # for i in range(len(arr)):
+    #     my_arr[arr[i]] -= 1
+    #     index_of_current = my_arr[arr[i]]
+    #     returning_arr[index_of_current] = arr[i]
+        
+    # for i in range(len(arr)):
+    #     arr[i] = returning_arr[i]
+
+    #### WITHOUT CREATING A NEW ARR 
+    ## SPACE COMPLEXITY OF (k) k = range
+    ## TIME COMPLEXITY OF ?? (k * ?) (k + n?)
+
+    j = 0
+    for k in range(len(my_arr)):
+        for _ in range(my_arr[k]):
+            arr[j] = k
+            j += 1
+            
     return arr
+
+
+print(count_sort([random.randint(0,20) for _ in range(10)],20))
+
+
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,8 +2,11 @@
 def linear_search(arr, target):
   
   # TO-DO: add missing code
+  for i in range(arr):
+    if arr[i] == target:
+      return arr[i]
 
-   return -1   # not found
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -13,11 +16,23 @@ def binary_search(arr, target):
     return -1 # array empty
     
   low = 0
-  high = len(arr)-1
+  high = len(arr)
+  middle_point = (high - low) // 2
 
   # TO-DO: add missing code
+  while middle_point > high or middle_point < low:
+    if arr[middle_point] == target:
+      return middle_point
+    elif arr[middle_point] > target:
+      high = middle_point - 1
+    else:
+      low = middle_point + 1
+    middle_point = (high - low) // 2
 
   return -1 # not found
+
+my_arr = [2,3,5,6,7,8]
+print(binary_search(my_arr,7))
 
 
 # STRETCH: write a recursive implementation of Binary Search 
