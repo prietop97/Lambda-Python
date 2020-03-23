ec480f75e457eb1b9595c4675433def2ea93a41d
Pedro Prieto <pedro.prieto003@gmail.com>
2020-03-23 07:21:33 -0400

    bst lru queue stack and dll

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,8 @@
 import sys
-sys.path.append('../queue_and_stack')
+sys.path.append('./queue_and_stack')
 from dll_queue import Queue
 from dll_stack import Stack
+import random
 
 
 class BinarySearchTree:
@@ -12,46 +13,168 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        node = BinarySearchTree(value)
+        current = self
+        prev = self
+        while current:
+            prev = current
+            if value < current.value:
+                current = current.left
+            elif value >= current.value:
+                current = current.right
+            
+        
+        if prev.value > value:
+            prev.left = node
+        elif prev.value <= value:
+            prev.right = node
+        
+
+
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        current = self
+        while current:
+            if current.value == target:
+                return True
+            elif current.value > target:
+                current = current.left
+            elif current.value < target:
+                current = current.right
+
+        return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        current = self
+        while current.right:
+            current = current.right
+
+        return current.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        def rec_childs(node,left,right):
+            if node is None:
+                return False
+
+            if node.left is not None:
+                rec_childs(left,left.left,left.right)
+            if node.right is not None:
+                rec_childs(right,right.left,right.right)
+
+            return cb(node.value)
+
+            if not left and not right:
+                return None
+
+        rec_childs(self,self.left,self.right)
+
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+
+        def dft_in_order(node):
+            if node.left is None and node.right is None:
+                print(node.value)
+                return
+
+            if node.left is not None:
+                dft_in_order(node.left)
+            print(node.value)
+            if node.right is not None:
+                dft_in_order(node.right)
+            
+        dft_in_order(self)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        queue = Queue()
+        queue.enqueue(node)
+        def bft_rec(queue):
+            if queue.len() <= 0:
+                return
+            current = queue.dequeue()
+            print(current.value)
+            if current.left is not None:
+                queue.enqueue(current.left)
+            if current.right is not None:
+                queue.enqueue(current.right)
+            return bft_rec(queue)
+            
+        bft_rec(queue)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        
+        def dft_rec(node):
+            print(node.value)
+
+            if node.left is None and node.right is None:
+                return
+
+            if node.left is not None:
+                dft_rec(node.left)
+            if node.right is not None:
+                dft_rec(node.right)
+
+        return dft_rec(node)
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
-        pass
+        def dft_rec(node):
+            print(node.value)
+
+            if node.left is None and node.right is None:
+                return
+
+            if node.left is not None:
+                dft_rec(node.left)
+            if node.right is not None:
+                dft_rec(node.right)
+
+        return dft_rec(node)
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        pass
+
+        def dft_rec(node):
+        
+            if node.left is None and node.right is None:
+                print(node.value)
+                return
+
+            if node.left is not None:
+                dft_rec(node.left)
+
+            if node.right is not None:
+                dft_rec(node.right)
+            
+            print(node.value)
+
+        return dft_rec(node)
+
+
+bst = BinarySearchTree(1)
+bst.insert(8)
+bst.insert(5)
+bst.insert(7)
+bst.insert(6)
+bst.insert(3)
+bst.insert(4)
+bst.insert(2)
+
+bst.post_order_dft(bst)
--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -52,41 +52,126 @@ class DoublyLinkedList:
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
-        pass
+        node = ListNode(value,None,self.head)
+        if not self.head:
+            self.head = node
+            self.tail = node
+        else:
+            self.head.insert_before(node)
+            self.head = node
+        self.length += 1
+        return node
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        pass
+        node = self.head
+        self.length -= 1
+        if not node.next:
+            self.head = None
+            self.tail = None
+            return  node.value
+
+        self.head = self.head.next
+        self.head.prev = None
+
+        return node.value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
-        pass
+        node = ListNode(value,self.tail,None)
+        if not self.tail:
+            self.head = node
+            self.tail = node
+        else:
+            old_tail = self.tail
+            old_tail.next = node
+            self.tail = node
+
+        self.length += 1
+        return self.tail
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        node = self.tail
+        if not node:
+            return
+
+        self.length -= 1
+        if not node.prev:
+            self.head = None
+            self.tail = None
+            return node.value
+
+        self.tail = node.prev
+        self.tail.next = None
+
+        
+        return node.value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if self.head is node:
+            return "Already head"
+
+        if self.tail is node:
+            self.remove_from_tail()
+
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            self.length -= 1
+
+        return self.add_to_head(node.value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if self.tail is node:
+            return node
+
+        if self.head is node:
+            self.remove_from_head()
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            self.length -= 1
+
+        return self.add_to_tail(node.value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+        if self.head is node:
+            self.remove_from_head()
+        elif self.tail is node:
+            self.remove_from_tail()
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            self.length -= 1
+            return node.value
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        if not self.head:
+            return None
+
+        current = self.head.next
+        maximum = self.head.value
+
+        while current:
+            if current.value > maximum:
+                maximum = current.value
+            current = current.next
+
+        return maximum
+        
+
+
