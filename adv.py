5fa40f7908b7c39bf70e0d9eb7ef1d09957ea2ae
Pedro Prieto <pedro.prieto003@gmail.com>
2020-06-03 19:07:56 -0400

    best path and cleanup

--- adv.py
+++ adv.py
@@ -2,7 +2,8 @@ from room import Room
 from player import Player
 from world import World
 from util import Queue,Stack
-from random import shuffle
+from random import shuffle,choice
+import utils
 
 import random
 from ast import literal_eval
@@ -11,74 +12,206 @@ from ast import literal_eval
 world = World()
 
 
-# You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
-
-# Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
-
-# Print an ASCII map
 world.print_rooms()
-
 player = Player(world.starting_room)
 
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
+
 traversal_path = []
-def bft(player,traversal_path,visited):
+
+def reverse_direction(direction):
+    if direction == "w":
+        return "e"      
+    if direction == "e":
+        return "w"   
+    if direction == "n":
+        return "s"         
+    if direction == "s":
+        return "n"
+
+def get_end_points(player,world):
+    my_hash = set()
     q = Queue()
-    q.enqueue([(player.current_room,None)])
-
-    while q.size() > 0:
-        path = q.dequeue()
-        current = path[-1][0]
-        if current not in visited:
-            break
-        else:
-            for next_v in current.get_exits():
-                new_path = path + [(current.get_room_in_direction(next_v),next_v)]
-                q.enqueue(new_path)
-    
-    for i in range(1,len(path)):
-        traversal_path.append(path[i][1])
-        player.travel(path[i][1])
-
-
-
-def dft(traversal_path,player,world):
-
+    q.enqueue(player.current_room)
     visited = set()
+    while q.size() > 0:
+        room = q.dequeue()
+        visited.add(room)
 
-    while len(visited) != len(world.rooms):
-        current = player.current_room
+        if len(room.get_exits()) == 1:
+            my_hash.add(room)
 
-        unvisited = []
-        possible_paths = current.get_exits()
+        for direction in room.get_exits():
+            next_room = room.get_room_in_direction(direction)
+            if next_room not in visited:
+                q.enqueue(next_room)
+
+    return my_hash
+
+my_hash = {}
+
+def get_mother_nodes(room):
+    
+    visited = set()
+    s = Stack()
+    s.push([(room,None)])
+
+    while s.size() > 0:
+        path = s.pop()
+        current = path[-1][0]
+        neighboors = current.get_exits()
+
         visited.add(current)
 
-        for path in possible_paths:
-            if current.get_room_in_direction(path) not in visited:
-                unvisited.append(path)
-        
-        unvisited.sort()
+        for direction in neighboors:
+            next_room = current.get_room_in_direction(direction)
+            if next_room in visited:
+                neighboors.remove(direction)
+
+        if len(neighboors) > 1:
+            return path
+
+
+        for direction in neighboors:
+            next_room = current.get_room_in_direction(direction)
+            new_path = path + [(next_room,direction)]
+            s.push(new_path)
+
+
+
+
+dead_end = get_end_points(player,world)
+for room in dead_end:
+    path = get_mother_nodes(room)
+    last = path[-1][0]
+    if last in my_hash:
+        my_hash[last].append(path)
+    else:
+        my_hash[last] = []
+        my_hash[last].append(path)
+
+def travel_dead_ends(player,room,visited,traversal_paths):
+    if room in my_hash:
+        for paths in my_hash[room]:
+            if paths[0][0] not in visited:
+                
+                reverse = []
+                normal = []
+                for i in range(len(paths) - 1,-1,-1):
+                    path = paths[i]
+                    direction = path[1]
+                    if direction:
+                        reverse_path = reverse_direction(direction)
+                        reverse.append(reverse_path)
+                        normal.append(direction)
+                    
+                normal.reverse()
+                for path in reverse:
+                    player.travel(path)
+                    traversal_path.append(path)
+                    visited.add(player.current_room)
+
+                if len(visited) == len(world.rooms):
+                    return
+
+                for path in normal:
+                    player.travel(path)
+                    traversal_path.append(path)
+                    visited.add(player.current_room)
+
+def bft(starting_room,exce):
+    visited = set()
+    q = Queue()
+    q.enqueue([starting_room])
+    paths = []
+    while q.size() > 0:
+        path = q.dequeue()
+        current = path[-1]
+        paths.append(path)
+        possible_paths = utils.get_neighbors(current)
+        for direction,room in possible_paths.items():
+            if room not in path and room != exce:
+                new_path = path + [room]
+                q.enqueue(new_path)
+            
+    paths.sort(key=len)
+    return len(paths[-1])
+
+# bft(world.rooms[108],world.rooms[81])
+# bft(world.rooms[137],world.rooms[81])
+                
+def travel_maze(player,world,traversal_path):
+    visited = set()
+    cache = {}
+    while len(visited) != len(world.rooms):
+        current = player.current_room
+        unvisited = []
+        visited.add(current)
+        travel_dead_ends(player,player.current_room,visited,traversal_path)
+        possible_paths = utils.get_neighbors(player.current_room)
+        end_loops = []  
+        others = []      
+
+        for direction,room in possible_paths.items():
+            if room not in visited:
+                unvisited.append([room,direction])
 
         if unvisited:
-            traversal_path.append(unvisited[-1])
-            next_v = unvisited.pop()
-            player.travel(next_v)
+            for room,direction in unvisited:
+                total = 0
+                possible_paths = utils.get_neighbors(player.current_room)
+                if possible_paths and len(possible_paths) > 1:
+                    for direction2,room2 in possible_paths.items():
+                        if room != room2: 
+                            check = utils.get_island_entrance(room,room2,player.current_room)
+                            if check:
+                                total += 1
+                                others.append([room,direction])
+                    if total == 0:
+                        end_loops.append([room,direction])
+            
+            if end_loops:
+                best_direction = None
+                best = None
+                length_of_best = float('inf')
+                for room,direction in end_loops:
+                    now = bft(room,player.current_room)
+                    if now < length_of_best:
+                        length_of_best = now
+                        best = room
+                        best_direction = direction
+                player.travel(best_direction)
+                traversal_path.append(best_direction)
+                visited.add(best)
+            elif others:
+                player.travel(others[0][1])
+                traversal_path.append(others[0][1])
+                visited.add(others[0][0])
         else:
-            if len(visited) == len(world.rooms):
-                break
-            bft(player,traversal_path,visited)
-    
+            utils.bft(player,traversal_path,visited)
+
     return traversal_path
+
+
+
+
+
+
+travel_maze(player,world,traversal_path)
+
+
+
+
+
+
+
+
+
+        
     
-dft(traversal_path,player,world)
+
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -86,21 +219,26 @@ visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
 
+    
+
 for move in traversal_path:
     player.travel(move)
     visited_rooms.add(player.current_room)
+    print(player.current_room.id)
+
 
 if len(visited_rooms) == len(room_graph):
     print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
-    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+    #print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+    print(f"{len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 
 
 
-# #######
-# # UNCOMMENT TO WALK AROUND
-# #######
+#######
+# UNCOMMENT TO WALK AROUND
+#######
 # player.current_room.print_room_description(player)
 # while True:
 #     cmds = input("-> ").lower().split(" ")
--- util.py
+++ util.py
@@ -24,20 +24,20 @@ class Stack():
     def size(self):
         return len(self.stack)
 
-map_file = "testing.txt"
-paths = open(map_file, "r")
+# map_file = "testing.txt"
+# paths = open(map_file, "r")
 
-content = paths.read()
-content_list = list(content.split())
+# content = paths.read()
+# content_list = list(content.split())
 
-print(content_list)
+# print(content_list)
 
 
-map_file2 = "testing2.txt"
-paths2 = open(map_file2, "r")
+# map_file2 = "testing2.txt"
+# paths2 = open(map_file2, "r")
 
-content2 = paths2.read()
-content_list2 = list(content2.split())
+# content2 = paths2.read()
+# content_list2 = list(content2.split())
 
-print(len(content_list))
-print(len(content_list2))
+# print(len(content_list))
+# print(len(content_list2))
